e225cf4478b8ff8774c51f6f0423f82d4ce76c47
Srinivas <saisrinivas868@gmail.com>
2022-09-26 21:05:41 +0530

    Crypto Track

---
+++ src/routes/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import CoinSearch from '../components/CoinSearch'
+import Trending from '../components/Trending'
+
+const Home = ({coins}) => {
+  return (
+    <div>
+      <CoinSearch coins={coins} />
+      <Trending />
+    </div>
+  )
+}
+
+export default Home
